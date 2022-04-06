cd46c8984d8764c7955c045d68eff14cb7f99c14
mfikrin <muhammad.fikrin1729@gmail.com>
2022-04-07 00:27:48 +0700

    feat : spawn orb and destroy orb

---
+++ Assets/Scripts/Managers/OrbManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+
+    public int spawnOrb; // index orb
+
+
+    public float spawnTime;
+    //public float spawnLifeTime; // destroy after spawnLifeTime second
+    public Transform[] spawnPoints;
+
+    [SerializeField]
+    public MonoBehaviour factory;
+    IFactory Factory { get { return factory as IFactory; } }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(Spawn), spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        if (playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // Menduplikasi enemy
+        Instantiate(Factory.FactoryMethod(spawnOrb), spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
