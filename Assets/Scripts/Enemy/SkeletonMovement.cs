11175c015e2cdccb03076a5d8e4ce6697fb33df0
moshval <shevamohammad10@gmail.com>
2022-04-07 13:30:36 +0700

    add skeleton shooting animation

---
+++ Assets/Scripts/Enemy/SkeletonMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class SkeletonMovement : MonoBehaviour
+{
+    Transform player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    UnityEngine.AI.NavMeshAgent nav;
+
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
+        nav = GetComponent <UnityEngine.AI.NavMeshAgent>();
+    }
+
+
+    void Update ()
+    {
+        nav.enabled = false;
+    }
+}
