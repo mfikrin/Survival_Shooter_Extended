11b9c3d9795bb6bb47fe0e0cee309f9022d7b1e3
mfikrin <muhammad.fikrin1729@gmail.com>
2022-04-08 15:17:49 +0700

    feat : orb done in wavemode

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -53,7 +53,7 @@ public class Player : MonoBehaviour
         //}
 
         UnityEngine.Debug.Log("MODE GAME");
-        modeGame = "Zen";
+        modeGame = "Wave";
         UnityEngine.Debug.Log(modeGame);
 
         playerName = "Playerss";
