fc78062ebebec08e64efa2341f22ba1a63565a7b
moshval <shevamohammad10@gmail.com>
2022-04-08 23:45:11 +0700

    init self-exploding mob

---
+++ Assets/Scripts/Enemy/RobotMovement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class RobotMovement : MonoBehaviour
+{
+    Transform player;
+    PlayerHealth playerHealth;
+    EnemyHealth RobotHealth;
+    UnityEngine.AI.NavMeshAgent nav;
+
+    public float speed = 7f;
+
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        playerHealth = player.GetComponent<PlayerHealth>();
+        RobotHealth = GetComponent<EnemyHealth>();
+        nav = GetComponent <UnityEngine.AI.NavMeshAgent>();
+        nav.speed = speed;
+        
+    }
+
+
+    void Update ()
+    {
+        //Memindahkan posisi player
+        //Debug.Log("Nav Speed" + nav.speed);
+        if (RobotHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {
+            nav.SetDestination(player.position);
+        }
+        else //Hentikan moving
+        {
+            nav.enabled = false;
+        }
+    }
+}
