5c53ea3976d2364a158a9431347dd968f6be8394
mfikrin <muhammad.fikrin1729@gmail.com>
2022-04-10 01:18:57 +0700

    fix : save scoreboard done

--- Assets/Scripts/ScoreBoard/ScoreSuddenDeathUI.cs
+++ Assets/Scripts/ScoreBoard/ScoreSuddenDeathUI.cs
@@ -3,28 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.Linq;
+using Newtonsoft.Json;
 
 public class ScoreSuddenDeathUI : MonoBehaviour
 {
     public RowSuddenDeathUI rowSuddenDeathUIOdd;
     public RowSuddenDeathUI rowSuddenDeathUIEven;
-    public ScoreManager scoreManager;
     public static int scoreSuddenDeathUI;
     public static TimeSpan TimeSpanSuddenDeathUI;
 
+    private ScoreSuddenDeathData scoreSuddenDeathData;
+
     private void Start()
     {
+        var SuddenDeathScoreJson = PlayerPrefs.GetString("SuddenDeathScores");
+
+        Debug.Log(SuddenDeathScoreJson);
+        if (SuddenDeathScoreJson.Equals(String.Empty))
+        {
+            Debug.Log("nulll");
+            scoreSuddenDeathData = new ScoreSuddenDeathData();
+        }
+        else
+        {
+            Debug.Log("not nulll");
+            scoreSuddenDeathData = JsonConvert.DeserializeObject<ScoreSuddenDeathData>(SuddenDeathScoreJson);
+        }
+
+        Debug.Log(scoreSuddenDeathData);
+
+        Debug.Log(Player.playerName);
+        Debug.Log(Player.modeGame);
 
         if (Player.playerName != null && Player.modeGame.Equals("SuddenDeath"))
         {
 
-            scoreManager.AddSuddenDeathScore(new ScoreSuddenDeath(Player.playerName, scoreSuddenDeathUI, TimeSpanSuddenDeathUI));
+            AddSuddenDeathScore(new ScoreSuddenDeath(Player.playerName, scoreSuddenDeathUI, TimeSpanSuddenDeathUI));
+            Player.playerName = null;
         }
 
-        if (scoreManager.GetScoreSudden() != null)
+        if (scoreSuddenDeathData != null)
         {
-            var scores = scoreManager.GetSuddenDeathHighScores().ToArray();
-            for (int i = 0; i < scores.Length; i++)
+            var scores = GetSuddenDeathHighScores().ToArray();
+
+
+            Debug.Log(scores.Length);
+            int max_display = scores.Length;
+
+            if (scores.Length > 5)
+            {
+                max_display = 5; // display 5 first rank
+            }
+
+            for (int i = 0; i < max_display; i++)
             {
                 if (i % 2 == 0)
                 {
@@ -45,4 +76,27 @@ public class ScoreSuddenDeathUI : MonoBehaviour
             }
         }
     }
+
+    public IEnumerable<ScoreSuddenDeath> GetSuddenDeathHighScores()
+    {
+        return scoreSuddenDeathData.SuddenDeathScoreList.OrderByDescending(x => x.score).ThenByDescending(x => x.time);
+    }
+
+    public void AddSuddenDeathScore(ScoreSuddenDeath score)
+    {
+        scoreSuddenDeathData.SuddenDeathScoreList.Add(score);
+    }
+
+    private void OnDestroy()
+    {
+        SaveScoreSuddenDeath();
+    }
+
+    public void SaveScoreSuddenDeath()
+    {
+        var SuddenDeathScoreJson = JsonConvert.SerializeObject(scoreSuddenDeathData);
+        UnityEngine.Debug.Log(SuddenDeathScoreJson);
+        PlayerPrefs.SetString("SuddenDeathScores", SuddenDeathScoreJson);
+    }
+
 }
