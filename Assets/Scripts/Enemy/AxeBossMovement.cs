4dc4e5bf9070bd36842bc7363b2b5afe48c987b7
moshval <shevamohammad10@gmail.com>
2022-04-09 14:24:41 +0700

    melee boss : add spinning atk

--- Assets/Scripts/Enemy/AxeBossMovement.cs
+++ Assets/Scripts/Enemy/AxeBossMovement.cs
@@ -7,28 +7,40 @@ public class AxeBossMovement : MonoBehaviour
     PlayerHealth playerHealth;
     EnemyHealth enemyHealth;
     UnityEngine.AI.NavMeshAgent nav;
-    float rotSpeed = 5.0f;
+     Animator anim;
+     
     float speed = 7f;
+    float detectionRange = 8f;
 
 
     void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-
+        anim = GetComponent <Animator> ();
         playerHealth = player.GetComponent<PlayerHealth>();
         enemyHealth = GetComponent<EnemyHealth>();
         nav = GetComponent <UnityEngine.AI.NavMeshAgent>();
+        // nav.updateRotation = false;
     }
 
 
     void Update ()
     {
+        float dist = detectionRange - Vector3.Distance(transform.position, player.transform.position);
+        if(dist >= 0){
+            anim.SetBool("PlayerIsNear",true);
+
+        }
+        else{
+            anim.SetBool("PlayerIsNear",false);
+        }
         //Memindahkan posisi player
         //transform.Rotate (0,0,50*Time.deltaTime);
         if (enemyHealth.currentHealth > 0 && ((playerHealth.currentHealth > 0 && Player.modeGame != "SuddenDeath") || (playerHealth.currentHealth == 0 && Player.modeGame.Equals("SuddenDeath"))))
         {
             Debug.Log("MASUK KE NAV MESHAN");
             nav.SetDestination(player.position);
+            
         }
         else //Hentikan moving
         {
