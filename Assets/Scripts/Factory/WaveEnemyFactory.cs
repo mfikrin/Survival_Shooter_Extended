11acc2edc6658a4af37848d0d69c628d5bb1246a
moshval <shevamohammad10@gmail.com>
2022-04-10 03:44:52 +0700

    init wave mode mobcount randomiser

--- Assets/Scripts/Factory/WaveEnemyFactory.cs
+++ Assets/Scripts/Factory/WaveEnemyFactory.cs
@@ -7,28 +7,11 @@ public class WaveEnemyFactory : MonoBehaviour, IFactory
 
     [SerializeField]
     public GameObject[] enemyPrefab;
-    public int[] enemyWeight;
-    [SerializeField] public enemyPool[] waves;
 
     public GameObject FactoryMethod(int tag)
     {
         GameObject enemy = enemyPrefab[tag];
         return enemy;
     }
-    public int GetWeight(int tag)
-    {
-        return enemyWeight[tag];
-    }
-
-    public int[] GetPool(int wave){
-        return waves[wave].enemyPrefabIndex;
-    }
-}
-
-[System.Serializable]
-public class enemyPool {
-
-    public int[] enemyPrefabIndex;
-    public int maxWeight;
 
 }
