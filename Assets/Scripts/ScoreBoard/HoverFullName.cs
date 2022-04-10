91805234558ae4dbf5e056b41440c5506de48110
mfikrin <muhammad.fikrin1729@gmail.com>
2022-04-10 10:41:19 +0700

    feat : clickable name in scoreboard to display full name (Wave mode)

---
+++ Assets/Scripts/ScoreBoard/HoverFullName.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverFullName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start()
+    {
+        
+    }
+
+    public void OnMouseOver()
+    {
+        
+    }
+
+    public void OnMouseExit()
+    {
+        
+    }
+}
