91d49cca66b38cb227520830472c624d9915d856
mfikrin <muhammad.fikrin1729@gmail.com>
2022-05-07 02:18:17 +0700

    chore : remove except Exutable folder

--- Assets/Scripts/Projectiles/SkeletonProjectile.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
-using UnityEngine.SceneManagement;
-using System;
-
-public class SkeletonProjectile : MonoBehaviour{
-    public GameObject bullet;
-}
